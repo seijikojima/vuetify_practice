b1ee54fe0b6930060e40807bb78bd55e364ea8d7
seijikojima <sabosan331@gmail.com>
2021-04-07 19:29:49 +0900

    first

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,11 @@
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    icons: {
+        iconfont: 'md',
+      },
+});
